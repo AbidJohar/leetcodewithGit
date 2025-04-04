ac17c44d5480a2f85ba3fab77d9efbaedede8aef
ShairbazAli <shairbazali8@gmail.com>
2025-04-04 17:23:00 +0500

    solved validanagram problem in py

---
+++ Array&Hashing/validAnaram/problem.py
@@ -0,0 +1,35 @@
+def Valid_Anm(s,t):
+    length1=len(s)
+    sorting=sorted(s)
+    sortin=sorted(t)
+    
+    length2=len(t)
+
+    if(length2==1):
+         if(sortin==sorting):
+               return True
+        
+
+    else:
+     for i in range(0,length1-1):
+        print
+        if(sorting[i]==sortin[i] and length1==length2):
+           
+           if(sortin==sorting):
+               return True
+           
+            
+            
+    return False      
+   
+
+    
+
+       
+
+
+
+
+# display=Valid_Anm("racecar","carrace")
+display=Valid_Anm("x","x")
+print(display)
